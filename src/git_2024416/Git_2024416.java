82ba05e2be4ae9921f522014fb077201b1c89c37
Breno Rodrigues <68665793+BrenoRFB@users.noreply.github.com>
2024-12-09 18:08:22 +0000

    Added a simple print

--- src/git_2024416/Git_2024416.java
+++ src/git_2024416/Git_2024416.java
@@ -14,7 +14,7 @@ public class Git_2024416 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Breno Rodrigues - 2024416");
     }
     
 }
