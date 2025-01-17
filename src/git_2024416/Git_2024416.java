061a9bd45e14a706c957f24cee1b6f74f7d2fe63
Breno Rodrigues <68665793+BrenoRFB@users.noreply.github.com>
2025-01-17 01:10:31 +0000

    Added code from previous homework
    Added code from previous homework

--- src/git_2024416/Git_2024416.java
+++ src/git_2024416/Git_2024416.java
@@ -4,6 +4,8 @@
  */
 package git_2024416;
 
+import inpututils.InputUtils;
+
 /**
  *
  * @author breno
@@ -15,6 +17,27 @@ public class Git_2024416 {
      */
     public static void main(String[] args) {
         System.out.println("Breno Rodrigues - 2024416");
+        
+        // imports a package with previously written code with "tools" to ask user for input
+        InputUtils myKB = new InputUtils();
+        
+        String userName; // stores user name
+        
+        /* Ask user for input (their name)
+           The method "askUserForText" from "InputUtils" is one of these "tools"
+           It is a loop that keeps asking for only text input and also 
+           has an error message that says that only text is allowed.
+        */
+        userName = myKB.askUserForText("Please, type in your name.");
+            
+        System.out.println("That is a nice name. Lets see how it would sound without vowels:"); //print a message saying what the program will do next
+        
+        // Replace vowels, both uppercase and lowercase with no characters
+        String nameNoVowels = userName.replaceAll("[AEIOUaeiou]","");
+        
+        //Prints the results 
+        System.out.println(nameNoVowels);
+        
     }
     
 }
